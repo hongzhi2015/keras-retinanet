fbf240b2a261707c0051b563282d9599f985b055
hongzhi2015 <hongzhi@ek12.com>
2018-04-17 15:43:36 +0800

    Remove unused code

--- keras_retinanet/utils/evalx/__init__.py
+++ keras_retinanet/utils/evalx/__init__.py
@@ -1,7 +1,7 @@
-from .evaluate import RawDiagnostic, CookedDiagnostic, evaluate, get_raw_diag
+from .evaluate import RawDiagnostic, CookedDiagnostic, get_model_detections, get_raw_diag
 from .plotsumm import plot_summ
 from .plotdetail import plot_detail
 
 __all__ = ['RawDiagnostic', 'CookedDiagnostic',
-           'evaluate', 'get_raw_diag',
+           'get_model_detections', 'get_raw_diag',
            'plot_summ', 'plot_detail']

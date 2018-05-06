de4abefd16177099835990001f84c0ca75313c87
hongzhi2015 <hongzhi@ek12.com>
2018-05-06 10:26:46 +0800

    Support no positive images

--- examples/dlib_xml_to_ssd_csv.py
+++ examples/dlib_xml_to_ssd_csv.py
@@ -8,9 +8,13 @@ with open(sys.argv[1], 'r') as f:
   soup = BeautifulSoup(f, 'xml')
   for img in soup.findAll('image'):
     file = img['file']
-    for box in img.findAll('box'):
-      xmin = box['left']
-      xmax = int(box['left']) + int(box['width'])
-      ymin = box['top']
-      ymax = int(box['top']) + int(box['height'])
-      csv.write('%s,%s,%s,%s,%s,1\n' % (os.path.basename(file), xmin, xmax, ymin, ymax))
+    boxes = img.findAll('box')
+    if len(boxes) == 0:
+      csv.write('%s,,,,,\n' % (os.path.basename(file)))
+    else:
+      for box in img.findAll('box'):
+        xmin = box['left']
+        xmax = int(box['left']) + int(box['width'])
+        ymin = box['top']
+        ymax = int(box['top']) + int(box['height'])
+        csv.write('%s,%s,%s,%s,%s,1\n' % (os.path.basename(file), xmin, xmax, ymin, ymax))
